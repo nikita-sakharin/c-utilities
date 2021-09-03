3f76a8beff473a88abec87e7ec49cf3c5fe3020a
Nikita <nikitasa1997@gmail.com>
2021-09-04 01:08:07 +0300

    next

--- include/system_config.h
+++ include/system_config.h
@@ -1,7 +1,14 @@
 #ifndef C_UTILITIES_SYSTEM_CONFIG_H
 #define C_UTILITIES_SYSTEM_CONFIG_H
 
+#include <stddef.h> // max_align_t
+
 #define CACHE_LINE_SIZE (64U)
+static_assert(
+    CACHE_LINE_SIZE >= alignof(max_align_t),
+    "CACHE_LINE_SIZE must be greater or equal than alignof(max_align_t)"
+);
+
 #define PAGE_SIZE (4096U)
 
 #endif // C_UTILITIES_SYSTEM_CONFIG_H
