6e08aebab716fe79ac60616c3fc2d96ca83418cb
Nikita <nikitasa1997@gmail.com>
2021-09-06 01:56:22 +0300

    next

--- include/c_utilities/compare.h
+++ include/c_utilities/compare.h
@@ -1,4 +1,19 @@
 #ifndef C_UTILITIES_COMPARE_H
 #define C_UTILITIES_COMPARE_H
 
+// naming convention ???
+inline bool compare_eq_int(const void * const s1, const void * const s2) {
+    return *(const int *) s1 == *(const int *) s2;
+}
+
+inline int compare_gt_int(const void * const s1, const void * const s2) {
+    const int x = *(const int *) s1, const int y = *(const int *) s2;
+    return (y > x) - (x > y);
+}
+
+inline int compare_lt_int(const void * const s1, const void * const s2) {
+    const int x = *(const int *) s1, const int y = *(const int *) s2;
+    return (y < x) - (x < y);
+}
+
 #endif // C_UTILITIES_COMPARE_H
