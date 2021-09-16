3bfa1024e0fff0e712922d11f1cacf77a9b4c2c9
Nikita <nikitasa1997@gmail.com>
2021-09-16 13:44:20 +0300

    next

--- include/c_utilities/bit.h
+++ include/c_utilities/bit.h
@@ -1,6 +1,6 @@
 #ifndef C_UTILITIES_BIT_H
 #define C_UTILITIES_BIT_H
 
-#define HAS_SINGLE_BIT(x) ((x) != 0 && ((x) & ((x) - 1)) == 0)
+#define HAS_SINGLE_BIT(x) ((x) != 0 && ((x) & (x) - 1) == 0)
 
 #endif // C_UTILITIES_BIT_H
