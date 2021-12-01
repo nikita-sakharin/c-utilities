aa3ed2fe30abd57e805d5025a876a73045478de0
Nikita <nikitasa1997@gmail.com>
2021-12-01 12:08:52 +0000

    next

--- include/c_utilities/bit.h
+++ include/c_utilities/bit.h
@@ -3,4 +3,23 @@
 
 #define HAS_SINGLE_BIT(x) ((x) != 0 && ((x) & ((x) - 1)) == 0)
 
+inline uint uhasSingleBit(register const uint x) {
+    return HAS_SINGLE_BIT(x);
+}
+
+inline ulong ulhasSingleBit(register const ulong x) {
+    return HAS_SINGLE_BIT(x);
+}
+
+inline ullong ullhasSingleBit(register const ullong x) {
+    return HAS_SINGLE_BIT(x);
+}
+
+inline uintmax_t umaxhasSingleBit(register const uintmax_t x) {
+    return HAS_SINGLE_BIT(x);
+}
+
+#define hasSingleBit(x) \
+    TYPE_GENERIC_UNSIGNED_INTEGER_FUNCTION_1(hasSingleBit, x)
+
 #endif // C_UTILITIES_BIT_H
