87c8ea2aaea817e4101e68781350bbb798532c8c
Nikita <nikitasa1997@gmail.com>
2021-12-01 12:09:26 +0000

    next

--- src/bit.c
+++ src/bit.c
@@ -1,1 +1,9 @@
-#include <c_utilities/bit.h>
+#include <stdint.h> // uintmax_t
+
+#include <c_utilities/bit.h> // *hasSingleBit
+#include <c_utilities/types.h> // uint, ullong, ulong
+
+extern inline uint uhasSingleBit(uint);
+extern inline ulong ulhasSingleBit(ulong);
+extern inline ullong ullhasSingleBit(ullong);
+extern inline uintmax_t umaxhasSingleBit(uintmax_t);
