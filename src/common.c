d368b8d656b805507c94d8033f04a76b9894f64a
Nikita <nikitasa1997@gmail.com>
2021-12-02 11:21:49 +0000

    next

--- src/common.c
+++ src/common.c
@@ -1,1 +1,8 @@
-#include <c_utilities/common.h>
+#include <stddef.h> // ptrdiff_t, size_t
+
+#include <c_utilities/common.h> // memSwap, ptrDiff, ptrOffset
+
+extern inline void *memSwap(void * restrict, void * restrict, size_t);
+
+extern inline ptrdiff_t ptrDiff(const void *, const void *, size_t);
+extern inline void *ptrOffset(const void *, size_t, size_t);
