470785b4eca3324e9d995844ca1eb8d380d24888
Nikita <nikitasa1997@gmail.com>
2021-12-21 10:53:15 +0000

    next

--- src/system_config.c
+++ src/system_config.c
@@ -1,4 +1,5 @@
 #include <assert.h> // assert
+#include <limits.h> // UINT_MAX
 #include <stdalign.h> // alignof
 #include <stddef.h> // max_align_t, ptrdiff_t, size_t
 #include <stdint.h> // SIZE_MAX
@@ -10,32 +11,36 @@
 #endif // _WIN32
 
 #include <c_utilities/bit.h> // hasSingleBit
-#include <c_utilities/system_config.h> // nprocessors*
+#include <c_utilities/system_config.h> // nprocessors*, ???
 #include <c_utilities/types.h> // uint, ulong
 
 // int or uint ???
 extern uint nprocessorsConf(void) {
-    // static uint returns = 0U;
-#ifdef _WIN32
-    SYSTEM_INFO systemInfo;
-    GetSystemInfo(&systemInfo);
-    return (uint) systemInfo.dwNumberOfProcessors;
-#else
-    register const long returns = sysconf(_SC_NPROCESSORS_CONF);
-    return returns < 0 ? 0U : (uint) returns;
-#endif
+    static uint returns = 0U;
+    if (returns == 0U) {
+#       ifdef _WIN32
+        SYSTEM_INFO systemInfo;
+        GetSystemInfo(&systemInfo);
+        returns = (uint) systemInfo.dwNumberOfProcessors;
+#       else
+        register const long value = sysconf(_SC_NPROCESSORS_CONF);
+        assert((ulong) value <= UINT_MAX);
+        returns = value < 0 ? 0U : (uint) value;
+#       endif // _WIN32
+    }
+    return returns;
 }
 
 extern uint nprocessorsOnln(void) {
-    // static uint returns = 0U;
-#ifdef _WIN32
+#   ifdef _WIN32
     SYSTEM_INFO systemInfo;
     GetSystemInfo(&systemInfo);
+    assert(systemInfo.dwNumberOfProcessors <= UINT_MAX);
     return (uint) systemInfo.dwNumberOfProcessors;
-#else
+#   else
     register const long returns = sysconf(_SC_NPROCESSORS_ONLN);
     return returns < 0 ? 0U : (uint) returns;
-#endif
+#   endif
 }
 
 // pagesize ???
@@ -43,13 +48,13 @@ extern size_t pageSize(void) {
     static size_t returns = 0U;
     if (returns != 0)
         return returns;
-#ifdef _WIN32
-#else
+#   ifdef _WIN32
+#   else
     register const long value = sysconf(_SC_PAGESIZE); // _SC_PAGE_SIZE ???
     assert(value >= (ptrdiff_t) alignof(max_align_t) &&
         (ulong) value <= SIZE_MAX && hasSingleBit((ulong) value)
     );
     returns = value < 0 ? 0U : (size_t) value;
-#endif // _WIN32
+#   endif // _WIN32
     return returns;
 }
