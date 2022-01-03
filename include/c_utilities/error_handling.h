fca79986fa9b7ec85bb8f0da4400185e74a84403
Nikita <nikitasa1997@gmail.com>
2022-01-03 11:46:43 +0000

    next

--- include/c_utilities/error_handling.h
+++ include/c_utilities/error_handling.h
@@ -5,19 +5,30 @@
 #include <stdio.h> // fprintf, perror, stderr
 #include <stdlib.h> // EXIT_FAILURE, exit
 
-#define EXIT_IF(condition, message) \
+#define EXIT_IF(condition) \
     do { \
         if ((condition)) { \
             fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
-            perror((message)); \
+            perror(__func__); \
             exit(EXIT_FAILURE); \
         } \
     } while (false)
 
+#ifdef NDEBUG
 #define GOTO_IF(condition, label) \
     do { \
         if ((condition)) \
             goto label; \
     } while (false)
+#else
+#define GOTO_IF(condition, label) \
+    do { \
+        if ((condition)) { \
+            fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+            perror(__func__); \
+            goto label; \
+        } \
+    } while (false)
+#endif // NDEBUG
 
 #endif // C_UTILITIES_ERROR_HANDLING_H
