9d30021b2826c80df413ec52cfe83c05eed8153a
Nikita <nikitasa1997@gmail.com>
2022-01-09 17:54:37 +0000

    next

--- src/bit.c
+++ src/bit.c
@@ -1,9 +1,10 @@
+#include <stdbool.h> // bool
 #include <stdint.h> // uintmax_t
 
 #include <c_utilities/bit.h> // *hasSingleBit
 #include <c_utilities/types.h> // uint, ullong, ulong
 
-extern inline uint uhasSingleBit(uint);
-extern inline ulong ulhasSingleBit(ulong);
-extern inline ullong ullhasSingleBit(ullong);
-extern inline uintmax_t umaxhasSingleBit(uintmax_t);
+extern inline bool uhasSingleBit(uint);
+extern inline bool ulhasSingleBit(ulong);
+extern inline bool ullhasSingleBit(ullong);
+extern inline bool umaxhasSingleBit(uintmax_t);
