56a47f12c9d6d60a00438305bd91d4ef5fca02c6
Nikita <nikitasa1997@gmail.com>
2022-01-12 20:12:46 +0000

    next

--- src/main2.c
+++ src/main2.c
@@ -13,7 +13,7 @@ static inline void *memSwap(
     register void * restrict s2,
     register size_t n
 ) {
-    register const void * const ptr1 = s1;
+    register void * const ptr1 = s1;
     for (; n > 0; --n, s1 = (uchar *) s1 + 1, s2 = (uchar *) s2 + 1) {
         register const uchar buffer = *(const uchar *) s1;
         *(uchar *) s1 = *(const uchar *) s2;
