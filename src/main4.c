a916577c7fbca21c989325a31d9fee11dbed900d
Nikita <nikitasa1997@gmail.com>
2022-01-21 19:27:25 +0000

    next

---
+++ src/main4.c
@@ -0,0 +1,55 @@
+#include <stdalign.h> // alignas
+#include <stddef.h> // size_t
+#include <stdio.h> // printf
+#include <stdlib.h> // free, malloc, rand
+#include <string.h> // memset
+
+#include <c_utilities/error_handling.h> // EXIT_IF
+#include <c_utilities/types.h> // uchar
+
+#define LEVEL1_DCACHE_LINESIZE ((size_t) 64U)
+
+static inline size_t min(register const size_t x, register const size_t y) {
+    return y < x ? y : x;
+}
+
+// ???
+// ???
+static inline void *memSwap(
+    register void * restrict s1,
+    register void * restrict s2,
+    register size_t n
+) {
+    register void * const ptr1 = s1;
+    while (n > 0) {
+        alignas(LEVEL1_DCACHE_LINESIZE) uchar buffer[LEVEL1_DCACHE_LINESIZE];
+        register const size_t offset = min(LEVEL1_DCACHE_LINESIZE, n);
+        memcpy(buffer, s1, offset);
+        memcpy(s1, s2, offset);
+        memcpy(s2, buffer, offset);
+        s1 = (uchar *) s1 + offset;
+        s2 = (uchar *) s2 + offset;
+        n -= offset;
+    }
+    return ptr1;
+}
+
+#define SIZE (1ULL << 30)
+
+int main(void) {
+    void * const restrict first = malloc(SIZE),
+        * const restrict second = malloc(SIZE);
+    EXIT_IF(first == NULL || second == NULL, "malloc");
+
+    memset(first, rand(), SIZE);
+    memset(second, rand(), SIZE);
+    memSwap(first, second, SIZE);
+    printf("first[0] = %hhu, second[0] = %hhu\n",
+        *(const uchar *) first,
+        *(const uchar *) second
+    );
+    free(first);
+    free(second);
+
+    return 0;
+}
