6b8769ec0db07ce0503c6d72885ba258b14646b6
Nikita <nikitasa1997@gmail.com>
2022-01-21 20:56:37 +0000

    next

--- src/main3.c
+++ src/main3.c
@@ -13,7 +13,7 @@ static inline size_t min(register const size_t x, register const size_t y) {
     return y < x ? y : x;
 }
 
-// ???
+// 4781714586
 // ???
 static inline void *memSwap(
     register void * restrict s1,
