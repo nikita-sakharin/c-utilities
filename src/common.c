1ff0d39b02bcb57f101ace0915bb0ce4cf44ae15
Nikita <nikitasa1997@gmail.com>
2022-02-02 20:41:55 +0000

    next

--- src/common.c
+++ src/common.c
@@ -5,6 +5,9 @@
 extern inline void *arrSwap(void * restrict, size_t, size_t, size_t);
 
 extern inline void *memSwap(void * restrict, void * restrict, size_t);
+extern inline bool memCompareSwap(void * restrict, void * restrict, size_t,
+    int (*cmp)(const void *, const void *)
+);
 
 extern inline ptrdiff_t ptrDifference(const void *, const void *, size_t);
 extern inline void *ptrOffset(const void *, size_t, size_t);
