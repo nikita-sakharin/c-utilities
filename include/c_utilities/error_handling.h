5f792885713cb599702808d2ed198671b80e284c
Nikita Sakharin <nikitasa1997@gmail.com>
2022-04-20 16:49:54 +0300

    Update error_handling.h

--- include/c_utilities/error_handling.h
+++ include/c_utilities/error_handling.h
@@ -1,24 +1,28 @@
 #ifndef C_UTILITIES_ERROR_HANDLING_H
 #define C_UTILITIES_ERROR_HANDLING_H
 
+#include <errno.h> // errno
 #include <stdbool.h> // false
-#include <stdio.h> // fprintf, perror, stderr
+#include <stdio.h> // fflush, fprintf, perror, stderr
 #include <stdlib.h> // EXIT_FAILURE, abort, exit
+#include <string.h> // strerror
 
 #define ABORT_IF(condition) \
     do { \
         if ((condition)) { \
             fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
             perror(__func__); \
+            fflush(stderr); \
             abort(); \
         } \
     } while (false)
 
-#define EXIT_IF(condition) \
+#define EXIT_IF(condition, argv0, ...) \
     do { \
         if ((condition)) { \
-            fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
-            perror(__func__); \
+            fprintf(stderr, "%s: ", argv0); \
+            fprintf(stderr, __VA_ARGS__); \
+            fprintf(stderr, ": %s\n", strerror(errno)); \
             exit(EXIT_FAILURE); \
         } \
     } while (false)
