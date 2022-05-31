c3f3bc78a66d8e1920183e135897ffbb55d070dc
Nikita Sakharin <nikitasa1997@gmail.com>
2022-05-31 21:29:16 +0300

    Update compare.c

--- src/compare.c
+++ src/compare.c
@@ -1,8 +1,19 @@
 #include <c_utilities/compare.h>
 
 extern inline bool isEqualInt(const void *, const void *);
-extern inline bool isGreaterEqualInt(const void *, const void *);
-extern inline bool isGreaterInt(const void *, const void *);
-extern inline bool isLessEqualInt(const void *, const void *);
-extern inline bool isLessInt(const void *, const void *);
+extern inline bool isEqualUint(const void *, const void *);
+
 extern inline bool isNotEqualInt(const void *, const void *);
+extern inline bool isNotEqualUint(const void *, const void *);
+
+extern inline bool isGreaterInt(const void *, const void *);
+extern inline bool isGreaterUint(const void *, const void *);
+
+extern inline bool isGreaterEqualInt(const void *, const void *);
+extern inline bool isGreaterEqualUint(const void *, const void *);
+
+extern inline bool isLessInt(const void *, const void *);
+extern inline bool isLessUint(const void *, const void *);
+
+extern inline bool isLessEqualInt(const void *, const void *);
+extern inline bool isLessEqualUint(const void *, const void *);
