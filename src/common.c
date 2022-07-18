04a384c8ec155cc4035f0f0e9118a5ea41f89ce6
Nikita <nikitasa1997@gmail.com>
2022-07-18 22:32:06 +0100

    arr* -> elem*

--- src/common.c
+++ src/common.c
@@ -2,17 +2,17 @@
 
 #include <c_utilities/common.h> // elem*, mem*, ptr*
 
-extern inline int elemCompare(const void * restrict, size_t, size_t, size_t,
-    int (*)(const void *, const void *)
+extern inline int elemCompare(const void * restrict, size_t, size_t,
+    size_t, int (*)(const void *, const void *)
 );
-inline void *elemCompareMax(const void * restrict, size_t, size_t, size_t,
-    int (*)(const void *, const void *)
+extern inline void *elemCompareMax(const void * restrict, size_t, size_t,
+    size_t, int (*)(const void *, const void *)
 );
-inline void *elemCompareMin(const void * restrict, size_t, size_t, size_t,
-    int (*)(const void *, const void *)
+extern inline void *elemCompareMin(const void * restrict, size_t, size_t,
+    size_t, int (*)(const void *, const void *)
 );
-extern inline int elemCompareSwap(const void * restrict, size_t, size_t, size_t,
-    int (*)(const void *, const void *)
+extern inline int elemCompareSwap(const void * restrict, size_t, size_t,
+    size_t, int (*)(const void *, const void *)
 );
 extern inline void *elemSwap(void * restrict, size_t, size_t, size_t);
 
