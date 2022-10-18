94862b85babaa3855c93e9ac4a23bd1d8b3fdc3b
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2022-10-18 19:06:37 +0100

    common.c reorder

--- src/common.c
+++ src/common.c
@@ -11,15 +11,15 @@ extern inline void *elemCompareMax(const void * restrict, size_t, size_t,
 extern inline void *elemCompareMin(const void * restrict, size_t, size_t,
     size_t, int (*)(const void *, const void *)
 );
-extern inline int elemCompareSwap(const void * restrict, size_t, size_t,
+extern inline bool elemCompareSwap(const void * restrict, size_t, size_t,
     size_t, int (*)(const void *, const void *)
 );
 extern inline void *elemSwap(void * restrict, size_t, size_t, size_t);
 
-extern inline void *memSwap(void * restrict, void * restrict, size_t);
 extern inline bool memCompareSwap(void * restrict, void * restrict, size_t,
     int (*)(const void *, const void *)
 );
+extern inline void *memSwap(void * restrict, void * restrict, size_t);
 
 extern inline ptrdiff_t ptrDifference(const void *, const void *, size_t);
 extern inline void *ptrDecrement(const void *, size_t);
