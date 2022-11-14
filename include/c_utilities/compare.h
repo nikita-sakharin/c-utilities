ba1434c23625c946937417f8d49924e82c9218fe
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2022-11-15 01:35:29 +0300

    Update compare.h

--- include/c_utilities/compare.h
+++ include/c_utilities/compare.h
@@ -14,7 +14,6 @@
 // #define CMP_LESS(x, y) (((y) < (x)) - ((x) < (y)))
 
 // short or shrt, str or string
-// ((uintptr) s1 & aligof(int) - 1) == 0 && ((uintptr) s2 & aligof(int) - 1) == 0
 // (((uintptr) s1 | (uintptr) s2) & aligof(int) - 1) == 0
 inline int cmpGreaterChar(
     register const void * const s1,
