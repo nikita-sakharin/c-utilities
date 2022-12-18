f247e3f078261dfba6ea824546b77dc3eeba48aa
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2022-12-18 17:41:12 +0300

    Update arithmetic.c

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,10 +1,10 @@
 #include <stdbool.h> // bool
 #include <stdint.h> // intmax_t, uintmax_t
 
-#include <c_utilities/arithmetic.h> // *absDiff, *clamp, *dim, *max, *min
+#include <c_utilities/arithmetic.h> // *absDiff, *clamp, *dim, *max, *min, *sign
 #include <c_utilities/types.h> // llong, uint, ullong, ulong
 
-extern inline uint absDiff(int, int);
+extern inline uint (absDiff)(int, int);
 extern inline uint uabsDiff(uint, uint);
 extern inline ulong labsDiff(long, long);
 extern inline ulong ulabsDiff(ulong, ulong);
