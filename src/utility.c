d5a8e927ad076c8f17e5a6d034b93552587ba68a
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-04-09 20:06:34 +0300

    Update utility.c

--- src/utility.c
+++ src/utility.c
@@ -22,8 +22,11 @@ extern inline bool memCompareSwap(void * restrict, void * restrict, size_t,
 );
 extern inline void *memSwap(void * restrict, void * restrict, size_t);
 
+extern inline int ptrCompare(const void *, const void *);
 extern inline ptrdiff_t ptrDifference(const void *, const void *, size_t);
 extern inline void *ptrDecrement(const void *, size_t);
 extern inline void *ptrIncrement(const void *, size_t);
+extern inline void *ptrMax(const void *, const void *);
+extern inline void *ptrMin(const void *, const void *);
 extern inline void *ptrMidpoint(const void *, const void *, size_t);
 extern inline void *ptrOffset(const void *, ptrdiff_t, size_t);
