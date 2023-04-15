eac4c89e56e07bac4703e023000442f9a9c9e302
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-04-15 18:04:16 +0300

    Update arithmetic.c

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -40,6 +40,15 @@ extern inline ullong ullclamp(ullong, ullong, ullong);
 extern inline intmax_t imaxclamp(intmax_t, intmax_t, intmax_t);
 extern inline uintmax_t umaxclamp(uintmax_t, uintmax_t, uintmax_t);
 
+extern inline int (compare)(int, int);
+extern inline uint ucompare(uint, uint);
+extern inline long lcompare(long, long);
+extern inline ulong ulcompare(ulong, ulong);
+extern inline llong llcompare(llong, llong);
+extern inline ullong ullcompare(ullong, ullong);
+extern inline intmax_t imaxcompare(intmax_t, intmax_t);
+extern inline uintmax_t umaxcompare(uintmax_t, uintmax_t);
+
 extern inline uint (dim)(int, int);
 extern inline uint udim(uint, uint);
 extern inline ulong ldim(long, long);
