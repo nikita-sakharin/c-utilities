49665832adebb7e62f1c51dc4252bce2631c71bd
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-04-29 14:23:42 +0100

    isEven,isOdd

--- src/bit.c
+++ src/bit.c
@@ -1,8 +1,26 @@
 #include <stdbool.h> // bool
-#include <stdint.h> // uintmax_t
+#include <stdint.h> // intmax_t, uintmax_t
 
-#include <c_utilities/bit.h> // *isPowerOfTwo
-#include <c_utilities/types.h> // uint, ullong, ulong
+#include <c_utilities/bit.h> // *
+#include <c_utilities/types.h> // llong, uint, ullong, ulong
+
+extern inline bool (isEven)(int);
+extern inline bool uisEven(uint);
+extern inline bool lisEven(long);
+extern inline bool ulisEven(ulong);
+extern inline bool llisEven(llong);
+extern inline bool ullisEven(ullong);
+extern inline bool imaxisEven(intmax_t);
+extern inline bool umaxisEven(uintmax_t);
+
+extern inline bool (isOdd)(int);
+extern inline bool uisOdd(uint);
+extern inline bool lisOdd(long);
+extern inline bool ulisOdd(ulong);
+extern inline bool llisOdd(llong);
+extern inline bool ullisOdd(ullong);
+extern inline bool imaxisOdd(intmax_t);
+extern inline bool umaxisOdd(uintmax_t);
 
 extern inline bool uisPowerOfTwo(uint);
 extern inline bool ulisPowerOfTwo(ulong);
