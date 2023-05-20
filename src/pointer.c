f2a8008c7270d93453cac505a8b4723d391269ee
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-05-20 17:58:03 +0100

    pointer.h

--- src/pointer.c
+++ src/pointer.c
@@ -3,9 +3,9 @@
 #include <c_commons/pointer.h> // ptr*
 
 extern inline int ptrCompare(const void *, const void *);
-extern inline ptrdiff_t ptrDifference(const void *, const void *, size_t);
 extern inline void *ptrDecrement(const void *, size_t);
 extern inline void *ptrIncrement(const void *, size_t);
+extern inline ptrdiff_t ptrDifference(const void *, const void *, size_t);
 extern inline void *ptrMax(const void *, const void *);
 extern inline void *ptrMin(const void *, const void *);
 extern inline void *ptrMidpoint(const void *, const void *, size_t);
