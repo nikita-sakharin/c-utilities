41a9c804b95f9f97f250dd217aba2ab56a05f059
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-05-21 20:21:35 +0100

    License

--- include/c-commons/binary_search.h
+++ include/c-commons/binary_search.h
@@ -1,10 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef C_COMMONS_BINARY_SEARCH_H
 #define C_COMMONS_BINARY_SEARCH_H
 
 #include <stddef.h> // size_t
 
-void *binarySearch(const void *, const void *, size_t, size_t, int (*)(const void *, const void *));
-void *lowerBound(const void *, const void *, size_t, size_t, int (*)(const void *, const void *));
-void *upperBound(const void *, const void *, size_t, size_t, int (*)(const void *, const void *));
+void *binarySearch(const void *, const void *, size_t, size_t,
+    int (*)(const void *, const void *)
+);
+void *lowerBound(const void *, const void *, size_t, size_t,
+    int (*)(const void *, const void *)
+);
+void *upperBound(const void *, const void *, size_t, size_t,
+    int (*)(const void *, const void *)
+);
 
 #endif // C_COMMONS_BINARY_SEARCH_H
