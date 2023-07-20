4d1f200f2fb4466c780bed57427e148694d057f6
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-07-20 13:15:28 +0100

    preprocessor.h

--- include/c_commons/preprocessor.h
+++ include/c_commons/preprocessor.h
@@ -17,15 +17,9 @@
 #ifndef C_COMMONS_PREPROCESSOR_H
 #define C_COMMONS_PREPROCESSOR_H
 
-// #define IS_ADD_OVERFLOW(x, y)
-// #define CHECK_ADD(x, y) (IS_SIGNED(x) ? (x) < 0 ? (y) < INTMAX_MIN - (x) : (y) > INTMAX_MAX - (x) : (y) > UINTMAX_MAX - (x))
-// #define CHECK_ADD(x, y) (IS_SIGNED(x) ? (x) < 0 ? (y) < INTMAX_MIN - (x) : (x) > 0 && (y) > INTMAX_MAX - (x) : (y) > UINTMAX_MAX - (x))
-// #define CHECK_ADD(x, y) (IS_SIGNED(x) ? (x) < 0 && (y) < INTMAX_MIN - (x) || (x) > 0 && (y) > INTMAX_MAX - (x) : (y) > UINTMAX_MAX - (x))
-// #define IS_MUL_OVERFLOW(x, y)
-// #define CHECK_MUL(x, y)
-// #define IS_NEG_OVERFLOW(x, y)
-// #define CHECK_NEG(x, y)
-// #define IS_SUB_OVERFLOW(x, y)
-// #define CHECK_SUB(x, y)
+#define IS_ADD_OVERFLOW(x, y) (IS_SIGNED((x) ^ (y)) ? (y) < 0 ? (x) < INTMAX_MIN - (y) : (x) > INTMAX_MAX - (y) : (x) > UINTMAX_MAX - (y)) // CHECK_ADD
+#define IS_MUL_OVERFLOW(x, y) 1 // CHECK_MUL
+#define IS_NEG_OVERFLOW(x) (IS_SIGNED(x) && (x) == INTMAX_MIN) // CHECK_NEG
+#define IS_SUB_OVERFLOW(x, y) (IS_SIGNED((x) ^ (y)) ? (y) < 0 ? (x) > INTMAX_MAX + (y) : (x) < INTMAX_MIN + (y) : (x) < (y)) // CHECK_SUB
 
 #endif // C_COMMONS_PREPROCESSOR_H
