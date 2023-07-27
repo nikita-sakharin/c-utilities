64cf693941fae3fe0bfc9e5c6f942058079397dd
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-07-27 23:44:56 +0100

    IS_SIGNED, IS_UNSIGNED

--- include/c_commons/preprocessor.h
+++ include/c_commons/preprocessor.h
@@ -23,7 +23,6 @@
 // #define ALL(x) ((x) | ~(x))
 // #define ALL(x) (~NONE(x))
 #define IS_SIGNED(x) (ALL(x) < 0LL)
-// #define IS_SIGNED(x) (!IS_UNSIGNED(x))
 // IS_ABS_OVERFLOW
 #define IS_ADD_OVERFLOW(x, y) (IS_SIGNED((x) ^ (y)) ? (y) < 0LL ? (x) < INTMAX_MIN - (y) : (x) > INTMAX_MAX - (y) : (x) > UINTMAX_MAX - (y))
 #define IS_DIV_OVERFLOW(x, y) (IS_SIGNED((x) ^ (y)) && (x) == INTMAX_MIN && (y) == -1LL)
@@ -39,7 +38,6 @@
 #define IS_NEG_OVERFLOW(x) (IS_SIGNED(x) && (x) == INTMAX_MIN)
 #define IS_SUB_OVERFLOW(x, y) (IS_SIGNED((x) ^ (y)) ? (y) < 0LL ? (x) > INTMAX_MAX + (y) : (x) < INTMAX_MIN + (y) : (x) < (y))
 #define IS_UNSIGNED(x) (ALL(x) >= 0LL)
-// #define IS_UNSIGNED(x) (!IS_SIGNED(x))
 // #define NONE(x) ((x) - (x))
 // #define NONE(x) ((x) & 0LL)
 // #define NONE(x) ((x) & ~(x))
